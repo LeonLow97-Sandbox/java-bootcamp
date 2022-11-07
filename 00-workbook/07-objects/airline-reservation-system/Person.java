7a9c427acede9f24d030d03121631b93f66ac8f8
lowjiewei <102566827+LeonLow97@users.noreply.github.com>
2022-11-07 17:41:29 +0800

    Completed Workbook on toString

--- 00-workbook/07-objects/airline-reservation-system/Person.java
+++ 00-workbook/07-objects/airline-reservation-system/Person.java
@@ -1,101 +1,86 @@
 import java.util.Arrays;
-
+ 
 public class Person {
-
+    
     private String name;
     private String nationality;
     private String dateOfBirth;
     private String[] passport;
-    private int seatNumber; 
-
-    public Person (String name, String nationality, String dateOfBirth, int seatNumber) {
+    private int seatNumber;
+ 
+    public Person(String name, String nationality, String dateOfBirth, int seatNumber) {
         this.name = name;
         this.nationality = nationality;
         this.dateOfBirth = dateOfBirth;
         this.seatNumber = seatNumber;
         this.passport = new String[3];
     }
-
-    public Person (Person source) {
+ 
+    public Person(Person source) {
         this.name = source.name;
         this.nationality = source.nationality;
         this.dateOfBirth = source.dateOfBirth;
         this.seatNumber = source.seatNumber;
         this.passport = Arrays.copyOf(source.passport, source.passport.length);
     }
-
+    
     public String getName() {
         return this.name;
     }
-    
+ 
     public String getNationality() {
         return this.nationality;
     }
-
+ 
     public String getDateOfBirth() {
         return this.dateOfBirth;
     }
-
+ 
     public int getSeatNumber() {
         return this.seatNumber;
     }
-
-    public String[] getPassport() {
-        return Arrays.copyOf(this.passport, this.passport.length);
-    }
-
+ 
     public void setName(String name) {
         this.name = name;
     }
-
+ 
     public void setNationality(String nationality) {
         this.nationality = nationality;
     }
-
+ 
     public void setDateOfBirth(String dateOfBirth) {
         this.dateOfBirth = dateOfBirth;
     }
-
+ 
     public void setSeatNumber(int seatNumber) {
         this.seatNumber = seatNumber;
     }
-
+ 
+    public String[] getPassport() {
+        return Arrays.copyOf(this.passport, this.passport.length);
+    }
     public void setPassport() {
         this.passport = new String[] {this.name, this.nationality, this.dateOfBirth};
     }
-
-    /**
-     *  Function name: applyPassport
-     *  @return (boolean)
-     *
-     *  Inside the function:
-     *     1. Returns a random boolean of true or false.
-     */
+ 
     public boolean applyPassport() {
-        boolean[] isApplicable = {true, false};
-        double randomNum = 2 * Math.random();
-        int randomInt = (int)randomNum;
-        return isApplicable[randomInt];
+        int number = (int) (Math.random() * 2);  
+        if (number == 0) {
+            return false;
+        } else {
+            return true;
+        }
     }
-
-    /**
-     *  Function name: chooseSeat
-     *
-     *  Inside the function:
-     *    1. Sets this.seat to a random number between 1 -- 11.
-     */
+ 
     public void chooseSeat() {
-        double randomNum = 1 + 10*Math.random();
-        int randomInt = (int)randomNum;
-        this.seatNumber = randomInt;
+        this.seatNumber = (int) (Math.random() * 11 + 1); 
     }
-
+ 
     public String toString() {
-        return 
-            "Name: " + this.name + "\n" + "Nationality: " + 
-            this.nationality + "\n" + "Date of Birth: " + 
-            this.dateOfBirth + "\n" + "Seat Number: " +
-            this.seatNumber + "\n" + "Passport: " + 
-            Arrays.toString(this.passport) + "\n";
+        return "Name: " + this.name + "\n"
+        +  "Nationality: " + this.nationality + "\n"
+        +  "Date of Birth: " + this.dateOfBirth + "\n"
+        +  "Seat Number: " + this.seatNumber + "\n"
+        +  "Passport: " + Arrays.toString(this.passport) + "\n";
     }
 }
